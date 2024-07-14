175613ffdca12fb3723c8dca46945c11b2988b68
Lucas <lsdeabre@gmail.com>
2024-07-14 16:04:55 -0300

    Menu created and add method remove

--- Linked Lists/main.py
+++ Linked Lists/main.py
@@ -9,18 +9,21 @@ def inserir():
     """
     while True:
         try:
-            color_choice = int(input("Insira a cor do cartão: \n1 - Verde\n2 - Amarelo\n0 - Voltar\n"))
-
-            if not color_choice:
+            color_choice = input("Insira a cor do cartão: \nV - Verde\nA - Amarelo\nX - Voltar\n").capitalize()
+            
+            if color_choice == "X":
                 break
-            #//TODO: Implements the search with the last number on the list for each color
+
+            if color_choice != "V" and color_choice != "A":
+                print("Cor inválida para cartões\n")
+                continue
 
             number_choice = int(input("Insira um número válido para o cartão:\n"))
             if number_choice < 0:
                 print("Valor inválido para cartões\n")
                 continue
 
-            if color_choice == 'A':
+            if color_choice == "A":
                 if number_choice < 200:
                     print("Valor incorreto para cartões Amarelos\n")
                     continue
@@ -34,8 +37,6 @@ def inserir():
         if not waiting_list.head:
             waiting_list.head = card
             continue
-        
-        waiting_list.inserirComPrioridade(card) if card.has_priority() else waiting_list.inserirSemPrioridade(card)
 
 def imprimirListaEspera():
     """
@@ -43,5 +44,39 @@ def imprimirListaEspera():
     """
     print(waiting_list)
 
-inserir()
-imprimirListaEspera()
+def atenderPaciente():
+    """
+    Call patient by the number of card or inform there are no patients
+    """
+    attend = waiting_list.remove_first_card()
+    if not attend:
+        return print("Não há nenhum paciente para ser atendido")
+    
+    print(f"Chamando paciente para atendimento! Número: {attend.number}")
+
+def main():
+    print("\nBem vindo ao Sistema de Atendimento\n---> desenvolvido por: Lucas Soares de Abreu")
+    print("="*45)
+
+    while True:
+        try:
+            print("\nEscolha uma das opções:")
+            print("1 - Adicionar paciente a fila")
+            print("2 - Mostrar pacientes na fila")
+            print("3 - Chamar Paciente")
+            print("4 - Sair")
+            action = int(input())
+
+            if action == 1: inserir()
+            if action == 2: imprimirListaEspera()
+            if action == 3: atenderPaciente()
+            
+            if action == 4: 
+                print("Finalizando...")
+                break
+
+        except ValueError:
+            print("Valor inserido incorretamente! Tente novamente\n")
+            continue
+
+main()
