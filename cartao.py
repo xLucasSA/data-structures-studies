2eaaf6bd1f0448c810c84424a1ea9a0c86cac746
Lucas <lsdeabre@gmail.com>
2024-07-14 02:37:19 -0300

    Create gitignore

--- cartao.py
+++
@@ -1,8 +0,0 @@
-class Card:
-    def __init__(self, number: int, color: str) -> None:
-        self.number = number
-        self.color = color
-        self.next = None
-
-    def __str__(self) -> str:
-        return f"{self.number} - {self.color}"
