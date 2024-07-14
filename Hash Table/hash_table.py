9b5548bb2f76c22a9e5e1e060457cbd51978ef0b
Lucas <lsdeabre@gmail.com>
2024-07-14 18:21:29 -0300

    Bug correction

--- Hash Table/hash_table.py
+++ Hash Table/hash_table.py
@@ -20,5 +20,5 @@ class Hash_table:
         if state.sigla == "DF":
             self.table[7].insert(state)
 
-        number_hash = (ord(state.sigla[0]) + ord(state.sigla[1])) // 10
+        number_hash = (ord(state.sigla[0]) + ord(state.sigla[1])) % 10
         self.table[number_hash].insert(state)
